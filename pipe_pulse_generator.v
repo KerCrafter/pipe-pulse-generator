4ef4aacd6d8c733f4b979e2cffd1e3d50e485424
KerCrafter <clemkerneur@gmail.com>
2025-11-06 21:05:49 +0100

    fix warnings

--- pipe_pulse_generator.v
+++ pipe_pulse_generator.v
@@ -1,6 +1,4 @@
-module pipe_pulse_generator #(
-    parameter WIDTH = 1
-)(
+module pipe_pulse_generator (
     input  wire clk,
     input  wire s,          // signal d’entrée (le signal à surveiller)
     input  wire pipe_in,    // entrée du pipeline
@@ -8,7 +6,7 @@ module pipe_pulse_generator #(
     input  wire reset         // reset asynchrone
 );
 
-    reg [WIDTH-1:0] shift_reg = {WIDTH{1'b0}};
+    reg shift_reg = 1'b0;
     reg pulse = 1'b0;
     reg s_prev = 1'b0;
 
@@ -17,25 +15,19 @@ module pipe_pulse_generator #(
 
     always @(posedge clk) begin
         if (reset) begin
-            shift_reg <= {WIDTH{1'b0}};
+            shift_reg <= 1'b0;
             pulse <= 1'b0;
             s_prev <= 1'b0;
         end else begin
             s_prev <= s;
 
             if (trigger) begin
-                if (WIDTH > 1)
-                    shift_reg <= {shift_reg[WIDTH-1:0], 1'b1};
-                else
-                    shift_reg <= 1'b1;
+                shift_reg <= 1'b1;
             end else begin
-                if (WIDTH > 1)
-                    shift_reg <= {shift_reg[WIDTH-1:0], 1'b0};
-                else
-                    shift_reg <= 1'b0;
+                shift_reg <= 1'b0;
             end
 
-            pulse <= shift_reg[WIDTH-1];
+            pulse <= shift_reg;
         end
     end
 
